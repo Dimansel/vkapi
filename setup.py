fcb27f4df212009f08c838017271cb1424f4d304
Elliptic <dimansel@zoho.eu>
2018-12-11 11:11:52 -0500

    Removed __pycache__ directories. Updated package description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name = 'vkapi',
       version = '0.1',
-      description = 'Python Vk API wrapper',
+      description = 'Python3 SDK for VKontakte API',
       url = 'https://yastrik.su',
       author = 'Elliptic',
       author_email = 'admin@yastrik.su',
